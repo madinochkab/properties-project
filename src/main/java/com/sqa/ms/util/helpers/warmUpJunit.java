ba0549a9bfbaac1ac0dadb2630e2e61ae160d9de
Madina Begumova <madinochkab@yahoo.com>
2016-04-20 18:33:01 -0700

    add new branches

--- src/main/java/com/sqa/ms/util/helpers/warmUpJunit.java
+++ src/main/java/com/sqa/ms/util/helpers/warmUpJunit.java
@@ -10,10 +10,15 @@
 
 package com.sqa.ms.util.helpers;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
+
 import java.util.Scanner;
 
-import org.junit.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 /**
  * warmUpJunit //ADDD (description of class)
@@ -28,7 +33,47 @@ import org.junit.*;
  *
  */
 public class warmUpJunit {
-	Scanner scanner = new Scanner(System.in);
+	// Get a char value from the user
+	public static char getChar(String question) {
+		return 0;
+
+	}
+
+	// Get a char value from the user, must be an acceptable value
+	public static char getChar(String question, char... c) {
+		return 0;
+
+	}
+
+	// Get a double value from the user
+	public static double getDouble(String question) {
+		return 0;
+
+	}
+
+	// Get a int from the user, must be in acceptable range value
+	public static int getInt(String question, int... acceptableNumbers) {
+		return 0;
+
+	}
+
+	// Get a int from the user, must be in acceptable range value
+	public static int getIntWithRange(String question, int intMin, int intMax) {
+		return intMax;
+
+	}
+
+	// Get a String from the user, must be an acceptable value
+	public static String getString(String question, String... s) {
+		return question;
+
+	}
+
+	// Get a int from the user, must be in acceptable range value
+	public static int getStringWithoutWorkds(String question, String... InvalidWords) {
+		return 0;
+
+	}
 
 	/**
 	 * @throws java.lang.Exception
@@ -43,6 +88,8 @@ public class warmUpJunit {
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
 	}
+
+	Scanner scanner = new Scanner(System.in);
 
 	/**
 	 * @throws java.lang.Exception
@@ -61,41 +108,6 @@ public class warmUpJunit {
 	@Test
 	public void test() {
 		fail("Not yet implemented");
-	}
-
-	// Get a double value from the user
-	public static double getDouble(String question) {
-
-	}
-
-	// Get a char value from the user
-	public static char getChar(String question) {
-
-	}
-
-	// Get a char value from the user, must be an acceptable value
-	public static char getChar(String question, char... c) {
-
-	}
-
-	// Get a String from the user, must be an acceptable value
-	public static String getString(String question, String... s) {
-
-	}
-
-	// Get a int from the user, must be in acceptable range value
-	public static int getInt(String question, int... acceptableNumbers) {
-
-	}
-
-	// Get a int from the user, must be in acceptable range value
-	public static int getIntWithRange (String question, intMin, intMax){
-
-	}
-
-	// Get a int from the user, must be in acceptable range value
-	public static int getStringWithoutWorkds(String question, String... InvalidWords) {
-
 	}
 
 	// Create your own helper method that you may find useful
