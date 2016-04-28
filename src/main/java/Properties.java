90bad4215d97c82edd9b1dfb98f1e3eef9fa1f46
Madina Begumova <madinochkab@yahoo.com>
2016-04-27 19:26:46 -0700

    dd

--- src/main/java/Properties.java
+++ src/main/java/Properties.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  *   File Name: Properties.java<br>
  *
@@ -5,7 +8,7 @@
  *   Java Boot Camp Exercise<br>
  *   Instructor: Jean-francois Nepton<br>
  *   Created: Apr 27, 2016
- *   
+ *
  */
 
 /**
@@ -14,12 +17,25 @@
  * //ADDD (description of core fields)
  * <p>
  * //ADDD (description of core methods)
- * 
- * @author      Siebenthal, Madina
- * @version     1.0.0
- * @since       1.0
+ *
+ * @author Siebenthal, Madina
+ * @version 1.0.0
+ * @since 1.0
  *
  */
 public class Properties {
+	public HashMap<String, String> convertLineContents(ArrayList<String> textFileLineContents) {
+		return null;
+	}
+
+	public void displayDataContents(HashMap<String, String> properties) {
+	}
+
+	public void displayDataLines(ArrayList<String> textFileLineContents) {
+	}
+
+	public ArrayList<String> evalTextDataLines(String textFileLoadLocation) {
+		return null;
+	}
 
 }
