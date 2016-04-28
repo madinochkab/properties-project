ea8b1d3e0d6f1e00f5571f148cbea9f57a94ec9a
Madina Begumova <madinochkab@yahoo.com>
2016-04-27 19:11:56 -0700

    addting properties files

---
+++ src/main/java/Properties.java
@@ -0,0 +1,25 @@
+/**
+ *   File Name: Properties.java<br>
+ *
+ *   Siebenthal, Madina<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Apr 27, 2016
+ *   
+ */
+
+/**
+ * Properties //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author      Siebenthal, Madina
+ * @version     1.0.0
+ * @since       1.0
+ *
+ */
+public class Properties {
+
+}
